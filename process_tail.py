4580adda2358a26a9143faa00df0473a897a6e74
Spencer Taylor <spencertaylor808@gmail.com>
2019-05-17 21:31:50 -0700

    Add keyboard interrupt exceptions

--- process_tail.py
+++ process_tail.py
@@ -5,31 +5,41 @@ import os
 import multiprocessing
 
 def tail(fn, sleep=0.1):
-  f = open(fn)
-  curino = os.fstat(f.fileno()).st_ino
-  f.seek(0, os.SEEK_END)
-  while True:
-    line = f.readline()
+  try:
+    f = open(fn)
+    curino = os.fstat(f.fileno()).st_ino
+    f.seek(0, os.SEEK_END)
+    while True:
+      line = f.readline()
 
-    if line:
-      sys.stdout.write(line)
-    else:
-      time.sleep(sleep)
-    
-    try:
-      if os.stat(fn).st_ino != curino:
-        f2 = open(fn)
-        f.close()
-        f = f2
-        curino = os.fstat(f.fileno()).st_ino
-    except IOError:
-      pass
+      if line:
+        sys.stdout.write(line)
+      else:
+        time.sleep(sleep)
+      
+      try:
+        if os.stat(fn).st_ino != curino:
+          f2 = open(fn)
+          f.close()
+          f = f2
+          curino = os.fstat(f.fileno()).st_ino
+      except IOError:
+        pass
+  except KeyboardInterrupt:
+    f.close()
+    return
       
 
 if __name__ == "__main__":
-  files = os.listdir('.')
-  tails = []
-  for fn in files:
-    p = multiprocessing.Process(target=tail, args=(fn,))
-    tails.append(p)
-    p.start()
+  try: 
+    files = os.listdir('.')
+    tails = []
+    for fn in files:
+      if os.path.isdir('./' + fn):
+        continue
+      p = multiprocessing.Process(target=tail, args=(fn,))
+      tails.append(p)
+      p.start()
+  except KeyboardInterrupt:
+    for p in tails:
+      p.terminate()
